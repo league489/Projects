dc19bf28ce3d7b6bea801c0b0dc36a3794cfeee7
league489 <107813326+league489@users.noreply.github.com>
2026-05-20 19:34:59 +0200

    Add files via upload

---
+++ Cleaning script/script.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import seaborn as sns
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="Input file for cleaning")
+parser.add_argument("output_file",help="Cleansed output file")
+args = parser.parse_args()
+
+
+file = pd.read_csv(args.input_file)
+#info o zbiorze
+print(file.info())
+num_cols = file.select_dtypes(include=['int64','float64']).columns
+fig1= plt.figure(figsize=(12,6))
+plt.scatter(file['housing_median_age'],file['households'] , alpha=0.5)
+fig1.savefig("househols by housing median age - scatter.png")
+plt.show()
+fig2= plt.figure(figsize=(12,6))
+plt.hist(file['total_rooms'],bins=50)
+fig2.savefig("total rooms - histogram.png")
+plt.show()
+fig3= plt.figure(figsize=(12,6))
+sns.heatmap(file[num_cols].corr(),annot=True,cmap='coolwarm')
+fig3.savefig("feature correlation - heatmap.png")
+plt.show()
+# #Usunięcie duplikatów 
+file = file.drop_duplicates()
+#Uzupełnianie wartości pustych
+file[num_cols] = file[num_cols].fillna(file[num_cols].mean()) #wypełnienie średnią NA dla kolumn numerycznych
+file = file.dropna()# Usunięcie pozostałych wierszy z brakami(kolumny nienumeryczne)
+num_cols = file.select_dtypes(include=['int64','float64']).columns #Ponowna selekcja kolumn numerycznych
+#Normalizacja min-max dla kolumn numerycznych + test min ! max
+for c in num_cols:
+    if file[c].min() != file[c].max():
+        file[c]= (file[c]-file[c].min())/(file[c].max()-file[c].min())
+    else:
+        continue
+file.to_csv(args.output_file)
+
